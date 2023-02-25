00cdb3a68e2cb6e9ef2f04407213fafadd7cf117
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2023-02-25 18:04:57 -0300

    sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Router } from "./Router";
+import { ThemeProvider } from 'styled-components'
+import { Normalize } from 'styled-normalize'
 import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
 
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+      <Normalize />
+    </ThemeProvider>
 
   )
 }
--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,4 +1,25 @@
-
+import { Container } from './styles'
+import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import menuImg from '../../assets/menu.svg'
 export function Sidebar() {
-  return <h1>Sidebar</h1>
+  return (
+    <Container>
+      <button>
+        <img src={menuImg} alt='menu abrir e fechar' />
+      </button>
+      <nav>
+        <ul>
+          <li>
+            <a href='#' className='active'>
+              <BurgerIcon />
+              <span>
+                Humbúrgueres
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </Container>
+  )
+
 }
