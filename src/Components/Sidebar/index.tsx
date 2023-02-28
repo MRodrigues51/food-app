94d7b9684c0e10673f5d2ca1655a09eac127737a
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2023-02-28 19:41:13 -0300

    menu lateral

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import { Container } from './styles'
 import { ReactComponent as BurgerIcon } from '../../assets/burger.svg'
+import { ReactComponent as PizzaIcon } from '../../assets/pizza.svg'
+import { ReactComponent as SodaPopIcon } from '../../assets/soda.svg'
+import { ReactComponent as IceCreamIcon } from '../../assets/ice-cream.svg'
 import menuImg from '../../assets/menu.svg'
+
 export function Sidebar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  function handleToggleMenu() {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <Container>
-      <button>
+    <Container isMenuOpen={menuOpen}>
+      <button type='button' onClick={handleToggleMenu}>
         <img src={menuImg} alt='menu abrir e fechar' />
       </button>
       <nav>
@@ -14,6 +25,30 @@ export function Sidebar() {
               <BurgerIcon />
               <span>
                 Humbúrgueres
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <PizzaIcon />
+              <span>
+                Pizzas
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <SodaPopIcon />
+              <span>
+                Bebidas
+              </span>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <IceCreamIcon />
+              <span>
+                Sobremesas
               </span>
             </a>
           </li>
--- src/Components/Sidebar/styles.tsx
+++ src/Components/Sidebar/styles.tsx
@@ -1,9 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Container = styled.aside`
+interface ContainerProps {
+  isMenuOpen: boolean
+}
+
+export const Container = styled.aside<ContainerProps>`
   background-color: ${({ theme }) => theme.colors.red};
 
-  width: 7.75rem;
+  ${({ isMenuOpen }) =>
+    isMenuOpen ? css`width: 16.3rem;` : css`width: 7.75rem;`
+  }
+
 
   padding: 2rem 0;
   overflow: hidden;
@@ -11,6 +18,8 @@ export const Container = styled.aside`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  transition: width 0.3s;
 
   button {
     background: none;
@@ -77,6 +86,52 @@ export const Container = styled.aside`
 
           span {
             color: ${({ theme }) => theme.colors.yellow};
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 720px){
+    position: fixed;
+    left: 0;
+    margin: 0;
+    bottom: 0;
+    z-index: 999;
+
+    width: 100%;
+    height: 5rem;
+    padding: 0 0;
+    overflow-y: auto;
+
+    button {
+      display: none;
+    }
+    nav {
+      height: 100%;
+
+      ul {
+        flex-direction: row;
+        align-items: center;
+      }
+      li {
+        a {
+          flex-direction: column;
+          padding: 0rem;
+
+          svg {
+            width: 3.25rem;
+            height: 3.25rem;
+          }
+
+          span {
+            display: none;
+          }
+
+          &.active {
+            &::after {
+              display: none;
+            }
           }
         }
       }
