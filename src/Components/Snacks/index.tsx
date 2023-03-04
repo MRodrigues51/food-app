7fbfab38567c4455ff987f57986b81ee27a8c937
MARCELO RODRIGUES <marcelorodriguesweb@gmail.com>
2023-03-03 21:58:11 -0300

    teste building

--- src/Components/Snacks/index.tsx
+++ src/Components/Snacks/index.tsx
@@ -13,24 +13,24 @@ export function Snacks({ snacks }: SnacksProps) {
   return (
     <Container>
       {
-        // !snacks.length ? (
-        //   [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
-        // ) : (
-        snacks.map((snack) =>
-        (
-          <div key={snack.id} className='snack'>
-            <h2>{snack.name}</h2>
-            <img src={snack.image} alt={snack.name} />
-            <p>{snack.description}</p>
-            <div>
-              <strong>{currencyFormat(snack.price)}</strong>
-              <button type='button'>
-                <FiPlus />
-              </button>
+        !snacks.length ? (
+          [1, 2, 3, 4].map((n) => <SkeletonSnack key={n} />)
+        ) : (
+          snacks.map((snack) =>
+          (
+            <div key={snack.id} className='snack'>
+              <h2>{snack.name}</h2>
+              <img src={snack.image} alt={snack.name} />
+              <p>{snack.description}</p>
+              <div>
+                <strong>{currencyFormat(snack.price)}</strong>
+                <button type='button'>
+                  <FiPlus />
+                </button>
+              </div>
             </div>
-          </div>
-        ))
-        // )
+          ))
+        )
       }
     </Container>
   )
